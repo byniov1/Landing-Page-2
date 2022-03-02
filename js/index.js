adfc3e1b828750b60ada7f686c0e8230461a8f99
Adam <bynio788@gmail.com>
2022-03-02 12:08:26 +0100

    Minor changes in scss

--- js/index.js
+++ js/index.js
@@ -4,10 +4,11 @@ const hamburger = document.querySelector('.navigation__hamburger');
 const navigation__list = document.querySelector('.navigation__list')
 
 hamburger.addEventListener('click', () => {
-  console.log('klikam w hamburger');
   navigation__list.classList.toggle('is-active')
 })
 
-navigation__list.addEventListener('click', () => {
-  navigation__list.classList.toggle('is-active');
-})
+
+
+// hamburger.addEventListener('click', () => {
+//   navigation__list.classList.toggle('is-active');
+// })
